b5f31f9c89cc8bff542f4d918bd12f91b9c29156
Abhishek Sawhney <sawhneyab@sawhneyab-a02.vmware.com>
2022-01-20 19:59:35 +0530

    pipeline file added

--- src/main/java/com/training/accountservice/service/TransactionService.java
+++ src/main/java/com/training/accountservice/service/TransactionService.java
@@ -28,12 +28,6 @@ public class TransactionService {
         accountEntity.setBalance(updatedBalance);
         accountRepository.save(accountEntity);
         Transaction response = transactionRepository.save(transactionMapper.toTransaction(transactionDto));
-       /* streamBridge.send("transaction-notification-topic", MessageBuilder
-                .withPayload(TransactionNotificationDto
-                        .builder()
-                        .transactionId(response.getTransactionId())
-                        .build())
-                .build());*/
         return response.getTransactionId();
     }
 }
